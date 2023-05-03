1905a94f44de73285d69d3cf2a27e825a798cc6f
chaninwit <120734528+chaninwit@users.noreply.github.com>
2023-05-03 12:16:42 +0700

    tailwind with login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,14 +1,51 @@
 
+import AuthPhoto from '../assets/auth.png'
 function LoginPage() {
   return (
-	<div>LoginPage</div> 
-  )
+    <div className="h-screen flex">
+      <div className="flex-1 hidden lg:flex justify-items-center items-center">
+        <div className='p-16'>
+          <img src={AuthPhoto} alt='banner' className='w-full'/>
+        </div>
+      </div>
+      {/* Rigth */}
+      <div className=" flex-1 flex items-center justify-center">
+        <div className="m-[100px] flex flex-col gap-8 w-[80%]">
+          <h1 className="text-8xl font-bold mb-12">Login1111111</h1>
+          <form className="flex flex-col gap-4">
+            <input
+              type="email"
+              placeholder="example@mail.com"
+              className="p-6 text-4xl outline-none rounded-lg border-2"
+            />
+            <input
+              type="password"
+              placeholder="password"
+              className="p-6 text-4xl outline-none rounded-lg border-2"
+            />
+            <button
+              type="submit"
+              className="mt-4 p-4 text-4xl bg-[#db4c3f] text-white rounded-lg
+               hover:bg-red-600"
+            >
+              Login
+            </button>
+          </form>
+          <div className="flex gap-4">
+            <p className="text-2xl text-gray-600 hover:text-[#db4c3f] cursor-pointer font-bold">
+              forgot password ?
+            </p>
+            <p className="text-2xl text-gray-600 ">
+              Don't have an account ?{" "}
+              <span className="hover:text-[#db4c3f] cursor-pointer font-bold">
+                Go to sign up
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
-// rfc
-// rfce
-// rafc
-// rafce
-
-//<LoginPage/> => <div>LoginPage</div> 
+export default LoginPage;
