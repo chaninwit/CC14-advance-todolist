e35ba97869c5c41378a419cb5584da557897f7cd
chaninwit <120734528+chaninwit@users.noreply.github.com>
2023-05-03 16:52:16 +0700

    spinner

--- src/components/Common/Spinner.jsx
+++ src/components/Common/Spinner.jsx
@@ -1,10 +1,41 @@
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
+import styled from 'styled-components';
 
-export  function Spinner() {
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <CircularProgress />
-    </Box>
-  );
+// สร้าง Commponent
+const Wrapper = styled.div`
+    position: relative;
+    height: 100vh;
+
+    & > .loading__container {
+        display: flex;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+`;
+
+export function Spinner() {
+    return (
+        <Wrapper>
+            <div className='loading__container'>
+                <CircularProgress />
+            </div>
+        </Wrapper>
+    );
+}
+
+{
+    /* <Box
+                sx={{
+                    display: 'flex',
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: `translate(-${50}% , -${50}%)`,
+                }}
+            >
+
+			</Box> */
 }
